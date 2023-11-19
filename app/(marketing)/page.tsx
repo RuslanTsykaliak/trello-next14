db88424e6bef2645773de5243372326140f32704
Mac <mac@MacBook-Pro-2.local>
2023-11-19 14:54:27 +0200

    two

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -45,7 +45,9 @@ const MarketingPage = () => {
           work forward.
         </div>
       </div>
-      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+      <div className={cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+      textFont.className
+      )}>
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team works is unique -
         accomplish it all with Taskify.
